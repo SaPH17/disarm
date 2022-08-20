b89ba4dd3f9ecb8ebc127b69820547d1158a9882
Luis Frentzen <luisfrentzen@gmail.com>
2022-08-20 15:42:57 +0700

    added react cdn and components

--- view/js/app.jsx
+++ view/js/app.jsx
@@ -1,7 +1,19 @@
-class App extends React.Component {
-  render() {
-    return <div>React App</div>;
-  }
-}
+const { useState } = React;
+console.log(React);
+
+const App = (props) => {
+  const [text, setText] = useState("hello");
+
+  return (
+    <div>
+      <h1>{text}</h1>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("app"));
