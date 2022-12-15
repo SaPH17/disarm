a5f25212fa2a3ffbe5e26051886e360347d8eead
Ferdinand Gunawan <ferdinandg066@gmail.com>
2022-12-15 14:02:30 +0700

    fix: change id to uuid

---
+++ server/models/base.go
@@ -0,0 +1,24 @@
+package models
+
+import (
+	"time"
+	"gorm.io/gorm"
+	"github.com/satori/go.uuid"
+)
+
+// Model a basic GoLang struct which includes the following fields: ID, CreatedAt, UpdatedAt, DeletedAt
+// It may be embedded into your model or you may build your own model without it
+//    type User struct {
+//      gorm.Model
+//    }
+type Base struct {
+	ID        uuid.UUID  `gorm:"type:uuid;primarykey;"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
+
+func (base *Base) BeforeCreate(tx *gorm.DB) (err error) {
+	base.ID = uuid.NewV4()
+	return
+}
