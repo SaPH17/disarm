da5a9f110cd82fea88ae6c0d77b9eba5ee643043
ferdinand066m <ferdinandg066@gmail.com>
2022-12-16 08:55:36 +0700

    feat: add email column to user

--- server/controllers/userController.go
+++ server/controllers/userController.go
@@ -12,6 +12,7 @@ import (
 
 func CreateUser(c *gin.Context) {
 	var body struct {
+		Email string `json:"email" binding:"required"`
 		Username string `json:"username" binding:"required"`
 		Password string `json:"password" binding:"required"`
 	}
@@ -24,6 +25,7 @@ func CreateUser(c *gin.Context) {
 	}
 
 	escapedUsername := html.EscapeString(strings.TrimSpace(body.Username))
+	escapedEmail := html.EscapeString(strings.TrimSpace(body.Email))
 	hashedPassword, hashingErr := bcrypt.GenerateFromPassword([]byte(body.Password), bcrypt.DefaultCost)
 	if hashingErr != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
@@ -32,7 +34,7 @@ func CreateUser(c *gin.Context) {
 		return
 	}
 
-	user, dbErr := models.Users.Create(escapedUsername, string(hashedPassword))
+	user, dbErr := models.Users.Create(escapedEmail, string(hashedPassword), escapedUsername)
 
 	if dbErr != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
