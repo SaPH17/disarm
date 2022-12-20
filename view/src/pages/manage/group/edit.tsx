4dca5ddbf2537989fd788d22316072a871100214
Stephanus Aditya Pratama Harjono <59709896+SaPH17@users.noreply.github.com>
2022-12-20 12:49:32 +0700

    added update group

--- view/src/pages/manage/group/edit.tsx
+++ view/src/pages/manage/group/edit.tsx
@@ -4,13 +4,20 @@ import GroupServices from '../../../services/group-services';
 import { useParams } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import PrimaryButton from '../../../components/primary-button';
+import { useEffect, useState } from 'react';
 
 const ManageGroupEdit = () => {
   const params = useParams();
   const { data: groupData } = useQuery(`groups/${params.id}`, () =>
     GroupServices.getOneGroup(params.id)
   );
-  const group = groupData || null;
+  const [group, setGroup] = useState<any>({});
+
+  useEffect(() => {
+    setGroup({
+      ...groupData,
+    });
+  }, [groupData]);
 
   const breadcrumbsPages = [
     {
@@ -28,9 +35,6 @@ const ManageGroupEdit = () => {
       <Breadcrumbs pages={breadcrumbsPages}></Breadcrumbs>
       <div className="flex flex-row justify-between">
         <div className="text-xl font-semibold">Edit Group</div>
-        <div className="flex flex-row justify-end">
-          <PrimaryButton content="Save Changes" type="submit" />
-        </div>
       </div>
       <EditGroupForm group={group} />
     </>
